cfc4406a4c2aa76d977b90f38d420639cdf69155
Sebastian Kalwa <sebastiankalwa@gmail.com>
2019-03-11 13:15:25 +0100

    merge pb settings

---
+++ src/Migrations/MergePandaBlackSettings.php
@@ -0,0 +1,40 @@
+<?php
+namespace PandaBlack\Migrations;
+
+use Plenty\Modules\Market\Settings\Contracts\SettingsRepositoryContract;
+use Plenty\Modules\Market\Settings\Models\Settings;
+
+class MergePandaBlackSettings
+{
+    public function __construct()
+    {
+
+    }
+
+    public function run()
+    {
+        /** @var SettingsRepositoryContract $settingsRepositoryContract */
+        $settingsRepositoryContract = pluginApp(SettingsRepositoryContract::class);
+
+        /** @var Settings[] $properties */
+        $properties = $settingsRepositoryContract->find('PandaBlack', 'property');
+
+        $settings = [];
+
+        foreach ($properties as $key => $property) {
+            if ($key === 0) {
+                $settings = $property->settings;
+            } else {
+                $settings = array_merge($settings, $property->settings);
+            }
+        }
+
+        foreach ($properties as $key => $property) {
+            if ($key === 0) {
+                $settingsRepositoryContract->update($settings, $property->id);
+            } else {
+                $settingsRepositoryContract->delete($property->id);
+            }
+        }
+    }
+}
