26f41573cb19c30146744d02caac663c2db4b618
Sebastian Kalwa <sebastiankalwa@gmail.com>
2019-03-12 11:39:51 +0100

    use credential store

--- src/Controllers/AppController.php
+++ src/Controllers/AppController.php
@@ -20,7 +20,7 @@ class AppController extends Controller
     public function authenticate($apiCall, $params = null, $productDetails = null)
     {
         $libCall = pluginApp(LibraryCallContract::class);
-        $token = $this->Settings->get('pbToken');
+        $token = $this->Settings->getCredential('pbToken');
 
         if ($token !== null) {
             if ($token['expires_in'] > time()) {
