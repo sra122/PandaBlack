413408426f8458b42de27783f346a8bd0d2a0dc5
Sravan <sravan.k.katta@student.fh-kiel.de>
2019-04-11 14:49:20 +0200

    changes to return value

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -175,7 +175,7 @@ class ContentController extends Controller
         $productDetails = $this->productDetails();
 
         //$productStatus = $this->productStatus($productDetails);
-        //$validProductsWithSKU = $this->generateSKU($productDetails['exportData']);
+        $validProductsWithSKU = $this->generateSKU($productDetails['exportData']);
 
         /*if(!empty($productStatus['validProductDetails'])) {
             $validProductsWithSKU = $this->generateSKU($productStatus['validProductDetails']);
@@ -185,6 +185,7 @@ class ContentController extends Controller
 
         $test = [
             'products' => $productDetails['exportData'],
+            'validProductsWithSku' => $validProductsWithSKU,
             'referrerId' => $this->Settings->get('orderReferrerId')
         ];
 
