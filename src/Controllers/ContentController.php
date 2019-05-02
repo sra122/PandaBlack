685f478d39e2ddfb51e331e7e8bb811d6b672dc7
Sravan <sravan.k.katta@student.fh-kiel.de>
2019-05-02 16:30:29 +0200

    changes to status tab

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -319,18 +319,12 @@ class ContentController extends Controller
                 foreach($attributes as $attributeKey => $attribute) {
                     if(!array_key_exists($attributeKey, $productDetail['attributes']) && $attribute['required']) {
                         if(!in_array($productDetail['product_id'], $missingAttributeProducts)) {
-                            $missingAttributeProducts['product_id'][$attributeKey] = $attribute['name'];
+                            $missingAttributeProducts[$productDetail['product_id']][$attributeKey] = $attribute['name'];
                             $unfulfilledData = true;
                             /*array_push($missingAttributeProducts, $productDetail['product_id']);
                             unset($productDetails['exportData'][$key]);
                             break;*/
                         }
-                    } elseif(!array_key_exists($productDetail['attributes'][$attribute[$attributeKey]], $attribute['values'])){
-                        $wrongAttributeMapping[$productDetail['product_id']][$attributeKey] = $attribute['name'];
-                    }
-
-                    if(count($test) <= 0 && $attribute['values'] !== null) {
-                        array_push($test, $attribute['values']);
                     }
                 }
             }
