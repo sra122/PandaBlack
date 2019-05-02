564d4ea6ab9d75457cf819fd19a54effc9762888
Sravan <sravan.k.katta@student.fh-kiel.de>
2019-05-02 13:14:12 +0200

    changes to return value for product Details

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -205,14 +205,14 @@ class ContentController extends Controller
         $app = pluginApp(AppController::class);
         $productDetails = $this->productDetails();
 
-        /*$productStatus = $this->productStatus($productDetails);
+        $productStatus = $this->productStatus($productDetails);
 
         if(!empty($productStatus['validProductDetails'])) {
             $validProductsWithSKU = $this->generateSKU($productStatus['validProductDetails']);
             $app->authenticate('products_to_pandaBlack', null, $validProductsWithSKU);
-        }*/
+        }
 
-        return $productDetails;
+        return $productStatus;
 
         /*$propertiesRepo = pluginApp(PropertyRepositoryContract::class);
         $propertyRelationRepo = pluginApp(PropertyRelationRepositoryContract::class);
