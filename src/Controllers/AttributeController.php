b1a5b37771d872eaf1872285b604b2fcebc16701
Sravan <sravan.k.katta@student.fh-kiel.de>
2019-05-06 13:25:04 +0200

    modification to attribute controller

--- src/Controllers/AttributeController.php
+++ src/Controllers/AttributeController.php
@@ -92,7 +92,8 @@ class AttributeController extends Controller
                         'relationValues' => [
                             [
                                 'lang' => 'de',
-                                'value' => 'test-value'
+                                'value' => 'test-value',
+                                'description' => 'test-description'
                             ]
                         ]
                     ]);
