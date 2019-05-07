0d72f9bb0e60de5b58a077a3431030af7b32fde1
Sravan <sravan.k.katta@student.fh-kiel.de>
2019-05-07 10:32:53 +0200

    changes to return entries

--- src/Controllers/AttributeController.php
+++ src/Controllers/AttributeController.php
@@ -133,7 +133,7 @@ class AttributeController extends Controller
 
         $propertiesList = $propertyRepo->listProperties(1, 50);
 
-        return $propertiesList->getOwnPropertyNames();
+        return $propertiesList['entries'];
     }
 
 
