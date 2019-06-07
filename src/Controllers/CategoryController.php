ca91547443d3aadaea617eee6dae042c1e8da0a4
Sravan <sravan.k.katta@student.fh-kiel.de>
2019-06-07 13:59:21 +0200

    category name structure

--- src/Controllers/CategoryController.php
+++ src/Controllers/CategoryController.php
@@ -191,4 +191,47 @@ class CategoryController extends Controller
 
         return $pbChildCategoryTree;
     }
+
+
+    public function getPBCategoriesAsDropdown()
+    {
+        $app = pluginApp(AppController::class);
+
+        $pbCategories = $app->authenticate('pandaBlack_categories');
+
+        $categoryTree = [];
+
+        if(isset($pbCategories)) {
+
+            foreach($pbCategories as $l1 => $l1Category)
+            {
+                if($l1Category['parent_id'] === null)
+                {
+                    foreach($pbCategories as $l2 => $l2Category) {
+                        $set = false;
+                        if($l2Category['parent_id'] == $l1)
+                        {
+                            foreach($pbCategories as $l3 => $l3Category)
+                            {
+                                if($l3Category['parent_id'] == $l2)
+                                {
+
+                                    foreach($pbCategories as $l4 => $child3Category) {
+                                        if($l4 == $l3) {
+                                            $set = true;
+                                        }
+                                    }
+
+                                    if($set) {
+                                        $categoryTree[$l3] = $l1Category['name'] . ' > ' . $l2Category['name'] . ' > ' . $l3Category['name'];
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return $categoryTree;
+        }
+    }
 }
