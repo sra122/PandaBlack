4bf7502dfc0a9ca0a3f38f7e6149052c53f9e847
Sravan <sravan.k.katta@student.fh-kiel.de>
2019-06-13 14:32:01 +0200

    changes to Attribute as Property saving automatically

--- src/Controllers/MappingController.php
+++ src/Controllers/MappingController.php
@@ -9,6 +9,9 @@
 namespace PandaBlack\Controllers;
 
 
+use PandaBlack\Helpers\SettingsHelper;
+use Plenty\Modules\Property\Contracts\PropertyNameRepositoryContract;
+use Plenty\Modules\Property\Contracts\PropertyRepositoryContract;
 use Plenty\Plugin\Controller;
 use Plenty\Plugin\Http\Request;
 
@@ -16,9 +19,83 @@ class MappingController extends Controller
 {
     public function mapping(Request $request)
     {
-        $mappingData = $request->get('mappingInformation');
+        $mappingInfos = $request->get('mappingInformation');
         $categoryId = $request->get('categoryId');
 
-        return $mappingData;
+        foreach($mappingInfos as $key => $mappingInfo)
+        {
+            if(array_reverse(explode('-', $mappingInfo))[0] == 'attribute' && $mappingInfo == 'Create Automatically') {
+
+                $attributeName = str_replace('-attribute', '', $mappingInfo);
+
+                if(!($this->checkPropertyExist($attributeName)) && ($this->propertyUnchanged($attributeName, (int)$categoryId))) {
+                    $this->createProperty($attributeName);
+                }
+            }
+        }
+    }
+
+
+    private function createProperty($propertyName)
+    {
+        /** @var PropertyRepositoryContract $propertyRepository */
+        $propertyRepository = pluginApp(PropertyRepositoryContract::class);
+
+        /** @var PropertyNameRepositoryContract $propertyNameRepository */
+        $propertyNameRepository = pluginApp(PropertyNameRepositoryContract::class);
+
+        $propertyData = [
+            'cast' => 'selection',
+            'typeIdentifier' => 'item',
+            'position' => 0,
+            'names' => [
+                [
+                    'lang' => 'de',
+                    'name' => $propertyName,
+                    'description' => ''
+                ]
+            ]
+        ];
+
+        $property = $propertyRepository->createProperty($propertyData);
+
+        foreach($propertyData['names'] as $propertyName) {
+            $propertyName['propertyId'] = $property->id;
+            $propertyName = $propertyNameRepository->createName($propertyName);
+        }
+    }
+
+
+    private function checkPropertyExist($propertyName)
+    {
+        /** @var PropertyNameRepositoryContract $propertyNameRepository */
+        $propertyNameRepository = pluginApp(PropertyNameRepositoryContract::class);
+
+        $propertyNamesArray = $propertyNameRepository->listNames();
+
+        if(in_array($propertyName, $propertyNamesArray)) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    private function propertyUnchanged($attributeName, $categoryId)
+    {
+        /** @var SettingsHelper $settingHelper */
+        $settingHelper = pluginApp(SettingsHelper::class);
+
+        $attributes = $settingHelper->get(SettingsHelper::ATTRIBUTES);
+
+        if(isset($attributes[$categoryId])) {
+            foreach($attributes[$categoryId] as $attribute) {
+                if($attribute->required && ($attribute->name == $attributeName)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
