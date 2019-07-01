51e9c1c93edbfecc6fa48ec3650e276e6273b027
Sravan <sravan.k.katta@student.fh-kiel.de>
2019-07-01 14:10:22 +0200

    changes to admin Notifications

--- resources/lib/pandaBlack_notications.php
+++ resources/lib/pandaBlack_notications.php
@@ -4,7 +4,7 @@ $client = new \GuzzleHttp\Client();
 
 $res = $client->request(
     'GET',
-    'https://pb.i-ways-network.org/notifications-api',
+    'https://pb.i-ways-network.org/notification-api/notifications',
     [
         'headers' => [
             'APP-ID' => 'Lr7u9w86bUL5qsg7MJEVut8XYsqrZmTTxM67qFdH89f4NYQnHrkgKkMAsH9YLE4tjce4GtPSqrYScSt7w558USrVgXHB',
