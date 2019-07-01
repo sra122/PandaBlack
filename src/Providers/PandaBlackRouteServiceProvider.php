ddba354071fdb719cda9beb7be8c714b4b44819f
Sravan <sravan.k.katta@student.fh-kiel.de>
2019-07-01 15:32:13 +0200

    testing notifications

--- src/Providers/PandaBlackRouteServiceProvider.php
+++ src/Providers/PandaBlackRouteServiceProvider.php
@@ -54,7 +54,7 @@ class PandaBlackRouteServiceProvider extends RouteServiceProvider
             $router->get('markets/panda-black/mapping-data', 'PandaBlack\Controllers\MappingController@fetchPropertiesInfo');
 
             //Notification
-            $router->get('markets/panda-black/notifications', 'PandaBlack\Controllers\MappingController@updateNotifications');
+            $router->get('markets/panda-black/notifications', 'PandaBlack\Controllers\MappingController@fetchNotifications');
             $router->post('markets/panda-black/remove-notification', 'PandaBlack\Controllers\MappingController@removeNotification');
         });
     }
