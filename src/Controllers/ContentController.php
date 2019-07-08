cc2b60fa8fd28db802f4acff9cd11cb3dec1d91e
Sravan <sravanece222@gmail.com>
2019-07-09 00:36:24 +0200

    changes to content controller

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -263,15 +263,14 @@ class ContentController extends Controller
         $app = pluginApp(AppController::class);
         $mapping = pluginApp(MappingController::class);
         $productDetails = $this->productDetails();
-        return $productDetails;
-        /*$productStatus = $this->productStatus($productDetails);
+        $productStatus = $this->productStatus($productDetails);
 
         if(!empty($productStatus['validProductDetails'])) {
             $validProductsWithSKU = $this->generateSKU($productStatus['validProductDetails']);
             $app->authenticate('products_to_pandaBlack', null, $validProductsWithSKU);
         }
         $productStatus['unfulfilledProducts']['admin'] = $mapping->updateNotifications()['admin'];
-        $this->settings->set(SettingsHelper::NOTIFICATION, $productStatus['unfulfilledProducts']);*/
+        $this->settings->set(SettingsHelper::NOTIFICATION, $productStatus['unfulfilledProducts']);
     }
 
     private function productStatus($productDetails)
