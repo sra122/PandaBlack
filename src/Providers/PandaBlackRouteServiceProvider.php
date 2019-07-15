fd00ca8035e2900010998d07b9fe9bc8a56d6b80
Sravan <sravan.k.katta@student.fh-kiel.de>
2019-07-15 15:43:55 +0200

    tesing pandablack orderreferrer

--- src/Providers/PandaBlackRouteServiceProvider.php
+++ src/Providers/PandaBlackRouteServiceProvider.php
@@ -31,8 +31,8 @@ class PandaBlackRouteServiceProvider extends RouteServiceProvider
             $router->get('markets/panda-black/pm-property-values', 'PandaBlack\Controllers\AttributeController@getPMPropertyValues');
 
             //Sending Content Actions
-            $router->post('markets/panda-black/products-data', 'PandaBlack\Controllers\ContentController@sendProductDetails');
-            $router->get('markets/panda-black/products-status', 'PandaBlack\Controllers\ContentController@sendProductDetails');
+            $router->post('markets/panda-black/products-data', 'PandaBlack\Controllers\ContentController@getReferrerId');
+            $router->get('markets/panda-black/products-status', 'PandaBlack\Controllers\ContentController@getReferrerId');
 
             //PandaBlack Category as Property
             $router->post('markets/panda-black/create-category-as-property', 'PandaBlack\Controllers\PropertyController@createCategoryAsProperty');
