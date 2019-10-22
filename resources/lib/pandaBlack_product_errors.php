72b08c8a6a2af3237ef72256b8fb635e69db408c
Sravan <sravan.k.katta@student.fh-kiel.de>
2019-10-22 16:32:44 +0200

    removing the version info

--- resources/lib/pandaBlack_product_errors.php
+++ resources/lib/pandaBlack_product_errors.php
@@ -2,18 +2,13 @@
 
 $client = new \GuzzleHttp\Client();
 
-$path = getcwd();
-$file = $path . '../../plugin.json';
-$version = json_decode(file_get_contents($file), true)['version'];
-
 $res = $client->request(
     'GET',
     'https://pb.i-ways-network.org/product-api/product-errors',
     [
         'headers' => [
             'APP-ID' => 'Lr7u9w86bUL5qsg7MJEVut8XYsqrZmTTxM67qFdH89f4NYQnHrkgKkMAsH9YLE4tjce4GtPSqrYScSt7w558USrVgXHB',
-            'API-AUTH-TOKEN' => SdkRestApi::getParam('token'),
-            'VERSION' => $version
+            'API-AUTH-TOKEN' => SdkRestApi::getParam('token')
         ]
     ]
 
