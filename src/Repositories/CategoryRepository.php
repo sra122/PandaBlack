bb1947fcd5b06241a8949bc438557f706bba7f6a
Sravan <sravan.k.katta@student.fh-kiel.de>
2019-10-31 13:03:58 +0100

    changes to category

--- src/Repositories/CategoryRepository.php
+++ src/Repositories/CategoryRepository.php
@@ -86,7 +86,7 @@ class CategoryRepository implements CategoriesRepositoryContract
 
     public function getCategories()
     {
-        $categoryData = $this->database->query(Categories::class)->orderBy('category_identifier');
+        $categoryData = $this->database->query(Categories::class)->orderBy('category_identifier')->limit(100);
 
         return $categoryData;
     }
