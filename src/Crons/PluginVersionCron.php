46f242441f4461bb08eb82698b07136c63bd952f
Sravan <sravan.k.katta@student.fh-kiel.de>
2019-10-31 09:19:44 +0100

    plugin version cron

---
+++ src/Crons/PluginVersionCron.php
@@ -0,0 +1,11 @@
+<?php
+namespace PandaBlack\Crons;
+use PandaBlack\Controllers\AppController;
+use Plenty\Modules\Cron\Contracts\CronHandler as Cron;
+class PluginVersionCron extends Cron
+{
+    public function __construct(AppController $appController)
+    {
+        $appController->authenticate('pandaBlack_version');
+    }
+}
