e425b2ba4ba6a570603cc8639d15ced076662d33
Sravan <sravan.k.katta@student.fh-kiel.de>
2019-11-07 08:22:25 +0100

    changes to notification route

--- src/Providers/PandaBlackRouteServiceProvider.php
+++ src/Providers/PandaBlackRouteServiceProvider.php
@@ -32,7 +32,6 @@ class PandaBlackRouteServiceProvider extends RouteServiceProvider
 
             //Sending Content Actions
             $router->post('markets/panda-black/products-data', 'PandaBlack\Controllers\ContentController@sendProductDetails');
-            //$router->get('markets/panda-black/products-status', 'PandaBlack\Controllers\MappingController@getProperties');
 
             //PandaBlack Category as Property
             $router->post('markets/panda-black/create-category-as-property', 'PandaBlack\Controllers\PropertyController@createCategoryAsProperty');
