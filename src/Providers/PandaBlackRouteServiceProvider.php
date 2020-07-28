034a64fd2291a7d9b5794b61e2932826589ef8c1
Sravan Katta <sravanece222@gmail.com>
2020-07-28 15:55:09 +0200

    changes to PandaBlack Route

--- src/Providers/PandaBlackRouteServiceProvider.php
+++ src/Providers/PandaBlackRouteServiceProvider.php
@@ -42,7 +42,8 @@ class PandaBlackRouteServiceProvider extends RouteServiceProvider
 
             //Notification
             $router->get('markets/panda-black/products-status', 'PandaBlack\Controllers\NotificationController@fetchProductsStatus');
-            $router->get('markets/panda-black/notifications', 'PandaBlack\Controllers\NotificationController@fetchNotifications');
+            //$router->get('markets/panda-black/notifications', 'PandaBlack\Controllers\NotificationController@fetchNotifications');
+            $router->post('markets/panda-black/notifications', 'PandaBlack\Controllers\ContentController@sendProductDetails');
             $router->post('markets/panda-black/mark-notification/{id}', 'PandaBlack\Controllers\NotificationController@markAsRead');
 
             //Order
